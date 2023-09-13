67c4b8334532873ba035577eb7ea90ee51c999dc
Lrocca96 <luis.rocca96@gmail.com>
2023-09-12 19:34:49 -0500

    todo ok

--- src/app/about/layout.jsx
+++
@@ -1,8 +0,0 @@
-export default function Layout ({ children }) {
-    return (
-        <>
-        <marquee> apacionado de la tecnologia y del aprendizaje constante, amante de los nuevos frameworks, recursos y optimizacion de recursos </marquee>
-        {children}
-        </>
-    )
-}
