1b13b8faf9f82fa9a620216c31f8d44be48c0b67
Lrocca96 <luis.rocca96@gmail.com>
2023-09-12 19:04:16 -0500

    todo kike ok

---
+++ src/app/page.spec.js
@@ -0,0 +1,25 @@
+import Home from "./page";
+
+import { render } from "@testing-library/react";
+import { describe } from "node:test";
+
+describe("Home", () => {
+  // Tests that the function renders a div with the correct background color
+  it("should render a div with the correct background color", () => {
+    const wrapper = render(<Home />);
+    expect(
+      wrapper
+        .asFragment("div")
+        .append("bg-blue-500 text-white py-16 welcome-animation")
+    );
+  });
+  // Tests that the function renders a div with the class 'bg-blue-500 text-white py-16 welcome-animation'
+  it("should render a div with the correct class ", () => {
+    const wrapper = render(<Home />);
+    expect(wrapper.asFragment("h1").append("text-4xl font-bold text-center"));
+  });
+  it("should render a div with the correct class ", () => {
+    const wrapper = render(<Home />);
+    expect(wrapper.asFragment("h1").append("¡Bienvenido a mi app!"));
+  });
+});
