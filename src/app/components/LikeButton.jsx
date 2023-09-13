4b43c6da3c1cc46b513b619d6b853591eaeb3ba2
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 11:18:06 -0500

    quitando espacion 2

--- src/app/components/LikeButton.jsx
+++ src/app/components/LikeButton.jsx
@@ -1,13 +1,13 @@
 'use client'
-import { useState } from 'react';
+import { useState } from 'react'
 
 export default function LikeButton () {
-    const [liked, setLiked] = useState(false)
-    return (
-        <>
-        <button onClick={() => setLiked(!liked)}>
-            {liked ? '💕' : '❤'}
-        </button>
-        </>
+  const [liked, setLiked] = useState(false)
+  return (
+    <>
+      <button onClick={() => setLiked(!liked)}>
+        {liked ? '💕' : '❤'}
+      </button>
+    </>
     )
 }
