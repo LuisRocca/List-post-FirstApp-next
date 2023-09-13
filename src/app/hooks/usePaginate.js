651337614fad8671b2dccbde6055c8fdfd1810d2
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 10:54:24 -0500

    ausepaginate

--- src/app/hooks/usePaginate.js
+++ src/app/hooks/usePaginate.js
@@ -1,10 +1,10 @@
-'use client';
+'use client'
 
-import { useState } from 'react';
-export function usePaginate(countPage) {
-  const [currentPage, setCurrentPage] = useState(0);
+import { useState } from 'react'
+export function usePaginate (countPage) {
+  const [currentPage, setCurrentPage] = useState(0)
   const pagination = (elements) => {
-    return elements.slice(currentPage, currentPage + countPage);
-  };
-  return { setCurrentPage, pagination, currentPage };
+    return elements.slice(currentPage, currentPage + countPage)
+  }
+  return { setCurrentPage, pagination, currentPage }
 }
