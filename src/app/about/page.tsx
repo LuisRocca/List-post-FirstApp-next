a4e3a431a38773ae6a39c415a133506be7a85311
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 11:36:32 -0500

    y ahora??

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Aspirations from '../components/Aspirations'
 import InterestingVideos from '../components/InterestingVideos'
 
-export default function About() {
+export default function About () {
   return (
   <section>
-    <h1 className='text-xl font-semibold'>Un poco sobre mi persona</h1>
-    <Aspirations />
-  <article>
-    <h1 className='text-xl font-semibold'>Videos Interesantes</h1>
-    <InterestingVideos />
-  </article>
+      <h1 className='text-xl font-semibold'>Un poco sobre mi persona</h1>
+      <Aspirations />
+    <article>
+      <h1 className='text-xl font-semibold'>Videos Interesantes</h1>
+      <InterestingVideos />
+    </article>
   </section>
   )
 }
