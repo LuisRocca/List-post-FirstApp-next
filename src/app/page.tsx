06e0662cff3447fd197eb56e34eddf007e476a9c
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 10:46:51 -0500

    page/app2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-
 export default function Home () {
   return (
     <div className='bg-blue-500 text-white py-16 welcome-animation'>
-    <h1 className='text-4xl font-bold text-center'>¡Bienvenido a mi app!</h1>
-    <p className='text-lg text-center mt-4'>Explora nuestras increíbles características.</p>
+     <h1 className='text-4xl font-bold text-center'>¡Bienvenido a mi app!</h1>
+     <p className='text-lg text-center mt-4'>Explora nuestras increíbles características.</p>
     </div>
   )
 }
