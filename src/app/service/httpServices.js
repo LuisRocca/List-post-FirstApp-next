dfab9dd79de331c7d8906e6afaf64a7dbeef1d4f
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 09:59:06 -0500

    12

--- src/app/service/httpServices.js
+++ src/app/service/httpServices.js
@@ -1,17 +1,11 @@
 const BASEURL = "https://jsonplaceholder.typicode.com/posts";
 
 export const getAllPost = () => {
-    return fetch(BASEURL)
-    .then(res => res.json())
-  }
+    return fetch(BASEURL).then(res => res.json())
+  };
 export const getPost = (id) => {
-    return fetch(`${BASEURL}/${id}`).then((res) =>
-      res.json()
-    );
+    return fetch(`${BASEURL}/${id}`).then((res) =>res.json())
   };
 export const getCommentsOfPost =  (id) => {
-    //  await new Promise(resolve => setTimeout(resolve, 3000))
-      return fetch(
-        `${BASEURL}/${id}/comments`
-      ).then((res) => res.json());
-    };
+      return fetch(`${BASEURL}/${id}/comments`).then((res) => res.json())
+  };
