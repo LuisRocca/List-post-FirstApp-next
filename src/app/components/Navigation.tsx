e116b59f58be2b6b1f1b072bfa655799cef045db
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 11:09:29 -0500

    navigationC2

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -9,13 +9,13 @@ export function Navigation () {
   return (
     <header>
       <nav className='border rounded-sm border-sky-500 bg-blue-500  mb-4'>
-          <div className='container mx-auto'>
-        <ul className='flex justify-end space-x-4'>
+      <div className='container mx-auto'>
+            <ul className='flex justify-end space-x-4'>
           {links.map(({ label, route }) => (
             <li className='bg-blue-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-blue-700' key={label}> <Link href={route}>{label}</Link></li>
           ))}
         </ul>
-      </div>
+          </div>
       </nav>
     </header>
   )
