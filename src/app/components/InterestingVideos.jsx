d95f30319e1b07d79dfd0411bd2f9c0e1f9efcc5
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 11:21:06 -0500

    videosinteresantes

--- src/app/components/InterestingVideos.jsx
+++ src/app/components/InterestingVideos.jsx
@@ -1,21 +1,21 @@
 'use client'
 
-import videos from '../moks/interestingVideos.json';
-import { usePaginate } from '../hooks/usePaginate';
-export default function InterestingVideos() {
-  const { setCurrentPage, pagination, currentPage } = usePaginate(2);
+import videos from '../moks/interestingVideos.json'
+import { usePaginate } from '../hooks/usePaginate'
+export default function InterestingVideos () {
+  const { setCurrentPage, pagination, currentPage } = usePaginate(2)
 
   const nextPage = () => {
-    console.log(currentPage);
-    if (currentPage >= 4) return;
-    setCurrentPage(currentPage + 2);
-  };
+    console.log(currentPage)
+    if (currentPage >= 4) return
+    setCurrentPage(currentPage + 2)
+  }
   const prevPage = () => {
-    if (currentPage <= 0) return;
-    setCurrentPage(currentPage - 2);
-  };
+    if (currentPage <= 0) return
+    setCurrentPage(currentPage - 2)
+  }
 
-  const postsPage = pagination(videos);
+  const postsPage = pagination(videos)
   return (
     <ul className='bg-slate-800 p-4 rounded-lg shadow-md mb-5 flex flex-col justify-items-center items-center'>
       <button
@@ -37,11 +37,11 @@ export default function InterestingVideos() {
             height='115'
             src={video.URLVideo}
             title={`${video.title}`}
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            allow='accelerometer autoplay clipboard-write encrypted-media gyroscope picture-in-picture web-share'
           ></iframe>
           <h4 className='text-xl font-semibold mt-5'>{video.title}</h4>
         </li>
       ))}
     </ul>
-  );
+  )
 }
