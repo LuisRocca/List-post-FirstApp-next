a9894ec942a07aac6a00213ec689faea2e145a53
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 10:28:07 -0500

    post/page

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Posts from '../components/Posts';
-import { getAllPost } from '../service/httpServices';
-export default async function PostsPage() {
-  const posts = await getAllPost();
-  return <Posts posts={posts} />;
+import React from 'react'
+import Posts from '../components/Posts'
+import { getAllPost } from '../service/httpServices'
+export default async function PostsPage () {
+  const posts = await getAllPost()
+  return <Posts posts={posts} />
 }
