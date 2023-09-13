20c64789beca4ef793ad60464320f699b90f0504
Lrocca96 <luis.rocca96@gmail.com>
2023-09-13 11:39:35 -0500

    ss

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,13 +4,13 @@ import InterestingVideos from '../components/InterestingVideos'
 
 export default function About () {
   return (
-  <section>
+      <section>
       <h1 className='text-xl font-semibold'>Un poco sobre mi persona</h1>
       <Aspirations />
-    <article>
-      <h1 className='text-xl font-semibold'>Videos Interesantes</h1>
-      <InterestingVideos />
-    </article>
-  </section>
+      <article>
+        <h1 className='text-xl font-semibold'>Videos Interesantes</h1>
+        <InterestingVideos />
+      </article>
+      </section>
   )
 }
